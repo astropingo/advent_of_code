c66843b65058d28f1e363f02237adb368fd65797
mvpetri <mvpetri@gmail.com>
2022-12-01 09:54:51 -0300

    Finished day 1, 2022

--- challenges/01/c02/adv_2022_01_02.py
+++ challenges/01/c02/adv_2022_01_02.py
@@ -7,12 +7,12 @@ di = import_input.DailyImput(usr="mvp")
 input_file = di.get_input()
 
 # Challenge
-highest_calories_pack = 0
+calories_list = []
 temp_pack = 0
 for line in input_file:
     if line.strip() != "":
         temp_pack += int(line.strip())
     else:
-        highest_calories_pack = max(temp_pack, highest_calories_pack)
+        calories_list.append(temp_pack)
         temp_pack = 0
-print(highest_calories_pack)
+print(sum(sorted(calories_list, reverse=True)[:3]))
