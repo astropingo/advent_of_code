3f4528f5a1720efe5fce5ad0764973653be5ed2c
Vinicius Rocha da Silva <silva.viniciusr@gmail.com>
2022-12-03 13:05:48 -0300

    Add input (vrs_03.txt) and solution for day3 Bonus solution 01 using pointers for string objects.

---
+++ challenges/03/c01/adv_2022_03_01-pointers.cpp
@@ -0,0 +1,77 @@
+/*
+ * adv_2022_03_01.cpp
+ *
+ *  Created on: 03 Dec 2022
+ *      Author: vsilva1
+ */
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define BUFFER_SIZE 81 //amount of characters read per line;
+#define NPOS std::string::npos //return value for substring not found
+
+#define INPUT_FILE "challenges/03/input/vrs_03.txt"
+std::fstream input_file;
+
+int read_ok() {
+    if (input_file.bad()) {
+        std::cerr << "Error reading from " << INPUT_FILE << "\n";
+        return -2;
+    } else if (input_file.eof()) {
+        std::cerr << "EOF: "<< INPUT_FILE << "\n";
+        return -1;
+    } else if (input_file.fail()) {
+        std::cerr << "Fail to read input to variable." << "\n";
+        return 0;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv) {
+
+    std::vector<char*> lines;
+    int answer = 0;
+
+    input_file.open(INPUT_FILE, std::ios_base::in); //open file in read-only mode;
+    if (read_ok() < 1) {
+        std::cerr << "Error opening " << INPUT_FILE << "\n";
+        input_file.close();
+        return -1;
+    }
+
+    //store rucksacks contents:
+    while (true) {
+        lines.push_back(new char[BUFFER_SIZE]);
+        input_file.getline(lines.back(), BUFFER_SIZE);
+        if (read_ok() < 1) { //eofbit || badbit || failbit
+            input_file.close();
+            break;
+        }
+    }
+    lines.pop_back(); //remove last element because it shall be empty;
+
+    //compute answer
+    for (std::vector<char*>::iterator it1 = lines.begin();
+            it1 != lines.end(); it1++){
+
+        std::string *s = new std::string(*it1);
+        std::string *s1 = new std::string(s->substr(0, s->length()/2));
+        std::string *s2 = new std::string(s->substr(s->length()/2, s->length()/2));
+        size_t pos = NPOS;
+        for (std::string::iterator it2 = s1->begin();
+                (it2 != s1->end() && pos == NPOS); it2++){
+            pos = s2->find(*it2);
+        }
+
+        if (pos == NPOS) continue;
+        islower((*s2)[pos]) ? answer += ((*s2)[pos] - 96) : answer += ((*s2)[pos] - 64 + 26);
+        delete s;
+        delete s1;
+        delete s2;
+    }
+
+    std::cout << "answer: " << answer << std::endl;
+    return 0;
+}
---
+++ challenges/03/c02/adv_2022_03_02.cpp
@@ -0,0 +1,92 @@
+/*
+ * adv_2022_03_02.cpp
+ *
+ *  Created on: 03 Dec 2022
+ *      Author: vsilva1
+ */
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define BUFFER_SIZE 81 //amount of characters read per line;
+#define NPOS std::string::npos //return value for substring not found
+
+#define INPUT_FILE "challenges/03/input/vrs_03.txt"
+std::fstream input_file;
+
+int read_ok() {
+    if (input_file.bad()) {
+        std::cerr << "Error reading from " << INPUT_FILE << "\n";
+        return -2;
+    } else if (input_file.eof()) {
+        std::cerr << "EOF: "<< INPUT_FILE << "\n";
+        return -1;
+    } else if (input_file.fail()) {
+        std::cerr << "Fail to read input to variable." << "\n";
+        return 0;
+    }
+    return 1;
+}
+
+size_t find_next_common_c(std::string &s1, std::string &s2, size_t &pos1) {
+    if (pos1 > 0) pos1++;
+    size_t pos2 = NPOS;
+    while (pos1 < s1.size() && pos2 == NPOS){
+        pos2 = s2.find(s1[pos1++]);
+    }
+    if (pos2 == NPOS) return s1.size();
+    return pos1 - 1;
+}
+
+int main(int argc, char **argv) {
+
+    std::vector<char*> lines;
+    int answer = 0;
+
+    input_file.open(INPUT_FILE, std::ios_base::in); //open file in read-only mode;
+    if (read_ok() < 1) {
+        std::cerr << "Error opening " << INPUT_FILE << "\n";
+        input_file.close();
+        return -1;
+    }
+
+    //store rucksacks contents:
+    while (true) {
+        lines.push_back(new char[BUFFER_SIZE]);
+        input_file.getline(lines.back(), BUFFER_SIZE);
+        if (read_ok() < 1) { //eofbit || badbit || failbit
+            input_file.close();
+            break;
+        }
+    }
+    lines.pop_back(); //remove last element because it shall be empty;
+    if (lines.size() % 3) {
+        std::cerr << "#lines not divisable by 3. Check input file: " << INPUT_FILE << "\n";
+        return -1;
+    }
+
+    //compute answer
+    for (std::vector<char*>::iterator itl = lines.begin();
+            itl != lines.end();){
+
+        std::string s1 = std::string(*(itl++));
+        std::string s2 = std::string(*(itl++));
+        std::string s3 = std::string(*(itl++));
+
+        size_t pos1 = 0;
+        size_t pos2 = NPOS;
+        do {
+            pos1 = find_next_common_c(s1, s2, pos1);
+            if (pos1 == s1.size()) {
+                std::cerr << "Could not find common item.\n";
+                return -1;
+            }
+            pos2 = s3.find(s1[pos1]);
+        } while (pos2 == NPOS);
+        islower(s1[pos1]) ? answer += (s1[pos1] - 96) : answer += (s1[pos1] - 64 + 26);
+    }
+
+    std::cout << "answer: " << answer << std::endl;
+    return 0;
+}
