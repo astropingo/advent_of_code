c580744694411eb7211b51ac7c3af9d343c9879c
mvpetri <mvpetri@gmail.com>
2022-12-03 03:13:33 -0300

    day3 deitou

--- challenges/03/c01/adv_2022_02_01.py
+++ challenges/03/c01/adv_2022_02_01.py
@@ -12,7 +12,13 @@ letters = string.ascii_letters
 
 def get_letter_point(s):
     mid_point = int(len(s)/2)
-    halves = (set(s[:mid_point]), set(s[mid_point:]))
-    return letters.index(list(halves[0].intersection(halves[1]))[0]) + 1
+    # Segments the string into two equal length halves and generate their sets
+    # Each set is a unique set of characters on each half. e.g.: set('abcacba') = {"a", "b", "c"}
+    halves_set = (set(s[:mid_point]), set(s[mid_point:]))
+
+    # Calculates the intersection between each set, an unique character
+    # and returns the amount of points for that unique letter.
+    # E.g: set('abcacba').intersection(set('pblkubp')) = {"b"}
+    return letters.index(halves_set[0].intersection(halves_set[1]).pop()) + 1
 
 print(sum(map(get_letter_point, [line for line in input_file])))
