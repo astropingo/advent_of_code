ace8876d34875a711d69e17f5e90f5a31e25ed5e
mvpetri <mvpetri@gmail.com>
2022-12-04 02:39:09 -0300

    renaming some files and solution for day 4 (python)

---
+++ challenges/04/c01/adv_2022_04_01.py
@@ -0,0 +1,15 @@
+# Hack to import the input file
+import sys
+sys.path.append("../../../utils")
+import import_input
+
+di = import_input.DailyImput(usr="mvp")
+input_file = di.get_input()
+
+# Challenge
+def check_overlap(s):
+    pairs = [list(map(int, d.split("-"))) for d in s.split(",")]
+    x1, x2, y1, y2 = pairs[0][0], pairs[1][0], pairs[0][1], pairs[1][1]
+    return (x1 <= x2 and y1 >= y2) or (x1 >= x2 and y1 <= y2)
+
+print(sum(map(check_overlap, [line for line in input_file])))
