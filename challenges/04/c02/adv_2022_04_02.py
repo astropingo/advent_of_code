d56dab6ff1fab8865cd3276a44cf4ec7ad8044a4
mvpetri <mvpetri@gmail.com>
2022-12-04 22:24:29 -0300

    fixing day2 solution (python)

--- challenges/04/c02/adv_2022_04_02.py
+++ challenges/04/c02/adv_2022_04_02.py
@@ -12,7 +12,8 @@ input_file = di.get_input()
 # Challenge
 def check_overlap(s):
     pairs = [list(map(int, d.split("-"))) for d in s.split(",")]
-    return (pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][1]) or (pairs[0][0] >= pairs[1][0] and pairs[0][1] <= pairs[1][1])
+    x1, x2, y1, y2 = pairs[0][0], pairs[1][0], pairs[0][1], pairs[1][1]
+    return (x1 <= y2 and y1 >= x2)
 
 print(sum(map(check_overlap, [line for line in input_file])))
 
